af6076a3c089a2540c0e5b89345391045ded7512
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-24 20:05:15 +0530

    .

--- React/NavBar_Routes/src/App.jsx
+++ React/NavBar_Routes/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
-import {Link ,Routes , Route ,NavLink} from "react-router-dom"
+import {Link ,Routes , Route ,NavLink, Navigate} from "react-router-dom"
 import Home from './Home'
 import About from './About'
 import Navbar from './Navbar'
@@ -18,7 +18,9 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        <Route path="/*" element={<PageNotFound/>}></Route>
+        {/* <Route path="/*" element={<PageNotFound/>}></Route> */}
+        <Route path="/*" element={<Navigate to="/login"/>}/>
+
       </Routes>
     </>
   )
