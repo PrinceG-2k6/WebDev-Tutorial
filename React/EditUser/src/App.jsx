b3cfca3340230241c1c772df1af02a56e47a9b1f
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-27 17:11:03 +0530

    .

--- React/EditUser/src/App.jsx
+++ React/EditUser/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import UserList from './UserList'
+import UserAdd from './UserAdd'
 // import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <div >
+        <ul className='NavList'>
+          <li><NavLink to="/">List</NavLink></li>
+          <li><NavLink to="/add">Add User</NavLink></li>
+        </ul>
+
+        <Routes>
+          <Route path='/' element={<UserList></UserList>}></Route>
+          <Route path='/' element={<UserAdd></UserAdd>}></Route>
+        </Routes>
+
+      </div>
     </>
   )
 }
