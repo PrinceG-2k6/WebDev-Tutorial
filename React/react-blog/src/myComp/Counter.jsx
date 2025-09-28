b5c389ec2cf89072fbfce0109d96f0595c857399
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-28 20:06:08 +0530

    Update Counter.jsx

--- React/react-blog/src/myComp/Counter.jsx
+++ React/react-blog/src/myComp/Counter.jsx
@@ -7,7 +7,7 @@ const Counter = () => {
 
   return (
     <>  
-       
+        <h1>DISPLAY TOGGLE</h1>
         <button onClick={()=>setDisplay(!display)}>Togggle</button>
         {display?
         <div>
