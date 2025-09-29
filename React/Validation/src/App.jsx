920dbfcb54da21c50817c7654ec0ba76ae318bb0
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-29 17:59:43 +0530

    .

--- React/Validation/src/App.jsx
+++ React/Validation/src/App.jsx
@@ -1,47 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState('');
+  const [nameErr, setNameErr] = useState("");
 
-  const [name,setName]=useState('');
-  const [nameErr,setNameErr]=useState("");
+  const [pass, setPass] = useState('');
+  const [passErr, setPassErr] = useState("");
 
-  const [pass,setPass]=useState('');
-  const [passErr,setPassErr]=useState("");
-
-  const handleName = (e)=>{
-    if(e.target.value.lenngth>5){
-      setNameErr("The name Should contain atmost 5 letter ");
-    }
-    else{
+  const handleName = (e) => {
+    const value = e.target.value;
+    setName(value);
+    if (value.length > 5) {
+      setNameErr("The name should contain at most 5 letters");
+    } else {
       setNameErr("");
     }
   }
 
-  const handlePass = (e)=>{
-    let regex = /^[A-Z0-9]+$/i;
-    if(regex.test(e.target.value)){
+  const handlePass = (e) => {
+    const value = e.target.value;
+    setPass(value);
+    const regex = /^[A-Z0-9]+$/i; // only letters and numbers
+    if (regex.test(value)) {
       setPassErr("");
-    }
-    else{
-      setPassErr("The Passwored Should Only Contain Aplhabets and Numbers");
+    } else {
+      setPassErr("The password should contain only alphabets and numbers");
     }
   }
 
   return (
     <>
-      <form action="">
+      <form>
         <label htmlFor="name">Name : </label>
-        <input type="text" name="name" id="name" placeholder='Enter Name' onChange={handleName} />
-        <span>{nameErr && nameErr}</span><br /><br />
+        <input
+          
+          type="text"
+          id="name"
+          placeholder="Enter Name"
+          onChange={handleName}
+          value={name}
+        />
+        <span className={nameErr?"error" :null}>{nameErr}</span><br /><br />
+
         <label htmlFor="Pass">Password : </label>
-        <input type="text" name="Pass" id="Pass" placeholder='Enter Pass' onChange={handlePass} />
-        <span>{passErr && passErr}</span>
+        <input
+          
+          type="text"
+          id="Pass"
+          placeholder="Enter Password"
+          onChange={handlePass}
+          value={pass}
+        />
+        <span className={passErr?"error" :null}>{passErr}</span>
         <br /><br />
-        <button disabled={nameErr || passErr}>Submit</button>
+
+        <button disabled={Boolean(nameErr) || Boolean(passErr)|| name && pass?null:true}>Submit</button>
       </form>
+
+      <h1>Name : {name}</h1>
+      <h1>Password : {pass}</h1>
     </>
   )
 }
