aca2cdc38473a119578ac80701dec11a704e83d2
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-29 17:50:17 +0530

    .

--- React/Validation/src/App.jsx
+++ React/Validation/src/App.jsx
@@ -4,30 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name,setName]=useState('');
+  const [nameErr,setNameErr]=useState("");
+
+  const [pass,setPass]=useState('');
+  const [passErr,setPassErr]=useState("");
+
+  const handleName = (e)=>{
+    if(e.target.value.lenngth>5){
+      setNameErr("The name Should contain atmost 5 letter ");
+    }
+    else{
+      setNameErr("");
+    }
+  }
+
+  const handlePass = (e)=>{
+    let regex = /^[A-Z0-9]+$/i;
+    if(regex.test(e.target.value)){
+      setPassErr("");
+    }
+    else{
+      setPassErr("The Passwored Should Only Contain Aplhabets and Numbers");
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form action="">
+        <label htmlFor="name">Name : </label>
+        <input type="text" name="name" id="name" placeholder='Enter Name' onChange={handleName} />
+        <span>{nameErr && nameErr}</span><br /><br />
+        <label htmlFor="Pass">Password : </label>
+        <input type="text" name="Pass" id="Pass" placeholder='Enter Pass' onChange={handlePass} />
+        <span>{passErr && passErr}</span>
+        <br /><br />
+        <button disabled={nameErr || passErr}>Submit</button>
+      </form>
     </>
   )
 }
