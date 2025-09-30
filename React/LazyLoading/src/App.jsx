35d4606c22a6414013302473f68696384868cd45
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-30 19:04:20 +0530

    .

--- React/LazyLoading/src/App.jsx
+++ React/LazyLoading/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const User = lazy(() => import('./user'));
+
 function App() {
+
+  const [load, setLoad] = useState(false)
 
   return (
     <>
-      
+      <div>
+        <h1>Lazy Loading</h1>
+        <button onClick={() => setLoad(true)}>Load user</button>
+        {
+          load ? <Suspense fallback={<h3>Loading.....</h3>}></Suspense> : null
+        }
+      </div>
     </>
   )
 }
