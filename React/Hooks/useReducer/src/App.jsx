8fb7332f05031bd294915e9c98555ed45bde7391
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-30 18:44:42 +0530

    .

--- React/Hooks/useReducer/src/App.jsx
+++ React/Hooks/useReducer/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
+import { act, useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const emptyData={
+  name:'',
+  password:"",
+  email:'',
+  city:'',
+  address:'' 
+}
+
+const reducer=(data,action)=>{
+  return {...data,[action.type]:action.val}
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [state,dispatch]=useReducer(reducer,emptyData)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>useReducer</h1>
+     <input type="text" onChange={(e)=>dispatch({val:e.target.value,type:'name'})} placeholder='Enter Name' />
+     <br /><br />
+     <input type="text" onChange={(e)=>dispatch({val:e.target.value,type:'password'})} placeholder='Enter Password' />
+     <br /><br />
+     <input type="text" onChange={(e)=>dispatch({val:e.target.value,type:'email'})} placeholder='Enter Email' />
+     <br /><br />
+     <input type="text" onChange={(e)=>dispatch({val:e.target.value,type:'city'})} placeholder='Enter City' />
+     <br /><br />
+     <input type="text" onChange={(e)=>dispatch({val:e.target.value,type:'address'})} placeholder='Enter Address' />
+     <br /><br />
+
+     <ul>
+      <li>Name : {state.name}</li>
+      <li>PassWord :{state.password}</li>
+      <li>Email : {state.email}</li>
+      <li>City : {state.city}</li>
+      <li>Address: {state.address}</li>
+     </ul>
+     <button>Add Details</button>
     </>
   )
 }
