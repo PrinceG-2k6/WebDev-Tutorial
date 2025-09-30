0b99aa9d5ab8682aa967e5b32d1fd343d5ecba78
PrinceG-2k6 <princegupta14311@gmail.com>
2025-09-30 19:29:21 +0530

    .

--- React/Hooks/useAPI/src/App.jsx
+++ React/Hooks/useAPI/src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense, use } from "react";
 
-function App() {
-  
-  
+const fetchUsers = () =>
+  fetch("http://localhost:3000/users").then((res) => res.json());
+
+const App = () => {
+  return (
+    <div>
+      <h1>Use API in React JS</h1>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Users />
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
+
+const Users = () => {
+  // use() works with a promise (experimental React feature)
+  const userData = use(fetchUsers());
 
   return (
     <>
-      
+      <h2>User list</h2>
+      {userData.users.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>Email: {user.email}</p>
+          <p>Age: {user.age}</p>
+        </div>
+      ))}
     </>
-  )
-}
-
-export default App
+  );
+};
