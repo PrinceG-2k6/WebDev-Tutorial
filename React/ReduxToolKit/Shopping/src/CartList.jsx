ee08398a66b48410042eda81dbaed87611c9d2fe
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-02 17:44:28 +0530

    .

--- React/ReduxToolKit/Shopping/src/CartList.jsx
+++ React/ReduxToolKit/Shopping/src/CartList.jsx
@@ -6,37 +6,40 @@ const CartList = () => {
 
     const dispatch = useDispatch();
 
-    const cartSelector = useSelector((state)=>state.cart.items);
+    const cartSelector = useSelector((state) => state.cart.items);
     console.log(cartSelector.length);
-  return (
-    <>
-    <div className="card-Header">
-        <h1>Your Cart Item</h1>
-        <span style={{marginLeft:'20px'}}>{cartSelector.length} Items</span>
-    </div>
-    <div className='cart-container'>
-          {
-            cartSelector.length>0 ?cartSelector.map((item,index)=>(
-              <div className="cart-item" key={index}>
-                <img src={item.thumbnail} />
-                <div className="content"><br />
-                  <div className="title">{item.title}</div>
-                  <div className="brand">{item.brand}</div>
-                  <div className="price" style={{color:'green'}}>Price: {item.price}</div>
-                  <div className="rating">Rating: {item.rating}</div>
-                    <button onClick={()=>dispatch(removeItem(item))} className='btn remove'>Remove From Cart</button>
-                  
+    return (
+        <>
+
+            <div className='cart-container'>
+                <div className="card-Header">
+                    <h2>Your Cart Item</h2>
+                    <span>{cartSelector.length} Items</span>
                 </div>
-              </div>
-            )):"No Item Added To Cart "
-          }
-          <div className='cart-footer'>
-            Total : {cartSelector.reduce((sum,item)=>sum+item.price,0)}
-        </div>
-        </div>
-        
+                {
+                    cartSelector.length > 0 ? cartSelector.map((item) => (
+                        <div className="cart-item" key={item.id}>
+                            <div className="item-info">
+                                <img src={item.thumbnail} />
+                                <div className="item-details">
+                                    <h4>{item.title}</h4>
+                                    <p>{item.brand}</p>
+                                </div>
+                            </div>
+                            <div className="item-actions">
+                                <span className='price'>{item.price}</span>
+                                <button onClick={() => dispatch(removeItem(item))} className='btn remove'>Remove From Cart</button>
+                            </div>
+                        </div>
+                    )) : "No Item Added To Cart "
+                }
+                <div className='cart-footer'>
+                    Total : {cartSelector.reduce((sum, item) => sum + item.price, 0)}
+                </div>
+            </div>
+
         </>
-  )
+    )
 }
 
 export default CartList
