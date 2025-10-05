5764e3c78a0bb95525a960a130eae2f6ce9b3353
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-05 20:46:00 +0530

    .

---
+++ NodeJs/L1_ServerCreation/color.js
@@ -0,0 +1,6 @@
+const colors = require("colors");
+
+console.log(colors.red("Hi I am red"));
+console.log(colors.yellow("Hi I am yellow"));
+console.log(colors.green("Hi I am green"));
+console.log(colors.blue("Hi I am blue"));
--- NodeJs/L1_ServerCreation/server.js
+++ NodeJs/L1_ServerCreation/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'text/plain'});
+    resp.writeHead(200,{'Content-Type':'text/html'});
     resp.write('<h1>Hello This Is Anil Sidhu</h1>')
     resp.write('<h2>Hello This Is Anil Sidhu</h2>')
     resp.end('Hello World\n');
