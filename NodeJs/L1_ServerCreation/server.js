2f8dd639187c5c1ac2834b9fedff3a1e2ec3d8fe
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-05 20:38:37 +0530

    .

--- NodeJs/L1_ServerCreation/server.js
+++ NodeJs/L1_ServerCreation/server.js
@@ -3,7 +3,6 @@ const http = require("http");
 http.createServer((req,resp)=>{
     resp.writeHead(200,{'Content-Type':'text/plain'});
     resp.write('<h1>Hello This Is Anil Sidhu</h1>')
-    
     resp.write('<h2>Hello This Is Anil Sidhu</h2>')
     resp.end('Hello World\n');
 }).listen(4800);
