dff030ea0e295b0bc618b29321181a2b0edac2be
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-05 21:00:16 +0530

    .

---
+++ NodeJs/L3_Nodemon/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+// Install : npm i nodemon
+// Helps to run server on changes 
+// Doesnt need to restart sever again and again
+
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'Content-Type':'text/html'});
+    resp.write('<h1>Hello This Is Prince gupta</h1>')
+    resp.write('<h2>Hello This Is Prince gupta</h2>')
+    
+    resp.write('<h3>Hello This Is Prince gupta</3>')
+    resp.end('Hello World\n');
+}).listen(4800);
+
+// nodemon server.js(in terminal)
