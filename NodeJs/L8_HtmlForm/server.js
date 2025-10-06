935d5ad6253539db05dc08c4b7b4870f1f2e46f0
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-06 18:24:08 +0530

    .

--- NodeJs/L8_HtmlForm/server.js
+++ NodeJs/L8_HtmlForm/server.js
@@ -28,6 +28,19 @@ http
         resp.write("<h1>Name : "+readAbleData.name+"</h1>")
         resp.write("<h1>Email :"+readAbleData.email+"</h1>")
         resp.end('<h1>Data Submitted</h1>');
+        let dataString ="My name is "+readAbleData.name+' And Email is '+readAbleData.email;
+        // fs.writeFileSync("text/"+readAbleData.name+'.txt', JSON.stringify(readAbleData, null, 2));
+        // console.log("File Created");
+        fs.writeFile("text/"+readAbleData.name+'.txt',dataString,'utf-8',(err)=>{
+          if(err){
+            resp.end("Internal Servor Error");
+            return;
+          }
+          else{
+            console.log("File Created");
+          }
+        });
+        
       });
     } else {
       resp.writeHead(404, { "Content-Type": "text/html" });
