2133b222c297738f9aa89d7a6bbeac262374719f
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-06 18:15:27 +0530

    .

--- NodeJs/L8_HtmlForm/server.js
+++ NodeJs/L8_HtmlForm/server.js
@@ -1,26 +1,36 @@
-import http from "http";
-import fs from "fs";
+const http = require("http");
+const fs = require("fs");
+const queryString = require("querystring");
 
 http
   .createServer((req, resp) => {
-    fs.readFile("./web.html", "utf-8", (err, data) => {
-      if (err) {
+    if (req.url == "/") {
+      fs.readFile("./web.html", "utf-8", (err, data) => {
+        if (err) {
+          resp.writeHead(500, { "Content-Type": "text/html" });
+          resp.end("Internal Server Error");
+          return;
+        }
         resp.writeHead(200, { "Content-Type": "text/html" });
-        resp.end("Internal Server Error");
-        return;
-      }
-      
+        resp.end(data);
+      });
+    } 
+    else if (req.url == "/submit" && req.method === "POST") {
+      let databody = [];
+      req.on('data', (chunk) => {
+        databody.push(chunk);
+      });
+      req.on('end', () => {
+        let rawData = Buffer.concat(databody).toString();
+        let readAbleData = queryString.parse(rawData);
+        console.log(readAbleData);
         resp.writeHead(200, { "Content-Type": "text/html" });
-      if (req.url == "/") {
-        resp.write(data);
-        
-      } 
-      else if (req.url == "/submit") {
-        resp.write('<h1>Data Submiteed</h1>')
-      }
-        resp.end();
-      
-    });
-
-    
+        resp.write("<h1>Name : "+readAbleData.name+"</h1>")
+        resp.write("<h1>Email :"+readAbleData.email+"</h1>")
+        resp.end('<h1>Data Submitted</h1>');
+      });
+    } else {
+      resp.writeHead(404, { "Content-Type": "text/html" });
+      resp.end("Not Found");
+    }
   }).listen(5600);
