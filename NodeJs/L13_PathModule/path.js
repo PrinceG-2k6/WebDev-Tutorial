63a820425f15bc9ecd147f8cd9005880826fc233
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-07 23:52:14 +0530

    .

--- NodeJs/L13_PathModule/path.js
+++ NodeJs/L13_PathModule/path.js
@@ -3,4 +3,5 @@ const file ="./peter.txt"
 // console.log(path.extname(file));
 // console.log(path.dirname(file));
 // console.log(path.basename(file));
-console.log(path.resolve("text","peter.txt"));
+// console.log(path.resolve("text","peter.txt"));
+console.log(path.isAbsolute(file));
