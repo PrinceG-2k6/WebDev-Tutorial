a1580586aa1e9857d2eed743e3b043ab0ac7979d
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-08 15:58:48 +0530

    .

---
+++ NodeJs/ExpressJs/L1_RenderHtml/pages/login.js
@@ -0,0 +1,9 @@
+export default function login(){
+    return  `<form action="/submit" method="post">
+    <br>
+    <input type="text" name="name" id="name" placeholder="Enter Name">
+    <br>
+    <input type="password" name="password" id="password" placeholder=" Enter password">
+    <br><br><button>Login</button>
+</form>`
+}
