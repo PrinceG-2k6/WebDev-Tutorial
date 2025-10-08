c3910d4f4421f573b044b16eee88e201dbba9b3a
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-08 15:25:24 +0530

    .

--- NodeJs/ExpressJs/index.js
+++ NodeJs/ExpressJs/index.js
@@ -1,11 +1,16 @@
-const express = require("express")
+import express from "express"
+import home from "./pages/home.js"
+import about from "./pages/about.js"
 
 const app=express()
 app.get("",(req,resp)=>{
     resp.send("<h1>Basic Node Js Example</h1>")
 })
 app.get("/about",(req,resp)=>{
-    resp.send("<h1>This is About Page</h1>")
+    resp.send(about())
+})
+app.get("/home",(req,resp)=>{
+    resp.send(home())
 })
 
 app.listen(4800)
