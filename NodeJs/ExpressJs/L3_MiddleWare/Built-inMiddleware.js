2ab45a02a7ea3ab4fda77ad4b930ae7f1f862e74
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-15 19:53:34 +0530

    .

---
+++ NodeJs/ExpressJs/L3_MiddleWare/Built-inMiddleware.js
@@ -0,0 +1,26 @@
+import express from "express"
+import path from "path"
+const app=express();
+const absPath = path.resolve("./View")
+const publicPath = path.resolve('public')
+
+
+
+
+
+app.use(express.static(publicPath));
+
+app.get("/",(req,resp)=>{
+    resp.sendFile(absPath+"/home.html")
+})
+app.get("/login",(req,resp)=>{
+    resp.sendFile(absPath+"/login.html")
+})
+app.post("/submit",(req,resp)=>{
+    resp.sendFile(absPath+"/submit.html")
+})
+app.use((req,resp)=>{
+    resp.status(404).sendFile(absPath+"/page404.html")
+})
+app.listen(4800)
+
