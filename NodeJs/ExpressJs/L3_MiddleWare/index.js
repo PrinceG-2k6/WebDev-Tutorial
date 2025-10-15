4c17cd3b7c8c22f76487b1f771cfbeb313823038
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-15 19:45:01 +0530

    .

--- NodeJs/ExpressJs/L3_MiddleWare/index.js
+++ NodeJs/ExpressJs/L3_MiddleWare/index.js
@@ -30,15 +30,21 @@ function AgeCheck(req, resp, next) {
   }
 }
 
+function checkURLRouteMiddleware(req, resp, next) {
+    console.log("This request is :",req.url)
+    next();
+  
+}
+
 // app.use(AgeCheck);
 
-app.get("/", (req, resp) => {
+app.get("/", checkURLRouteMiddleware,(req, resp) => {
   resp.send("<h1>home page</h1>");
 });
-app.get("/login", (req, resp) => {
+app.get("/login",checkURLRouteMiddleware,(req, resp) => {
   resp.send("<h1>Login page</h1>");
 });
-app.get("/admin", AgeCheck, (req, resp) => {
+app.get("/admin", AgeCheck, checkURLRouteMiddleware,(req, resp) => {
   resp.send("<h1>Admin page</h1>");
 }); 
 app.use((req, resp) => {
