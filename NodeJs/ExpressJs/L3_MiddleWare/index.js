0577f3443240c42e823f8dee6f3df03ac1617c3e
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-15 14:57:56 +0530

    .

--- NodeJs/ExpressJs/L3_MiddleWare/index.js
+++ NodeJs/ExpressJs/L3_MiddleWare/index.js
@@ -1,6 +1,6 @@
-import express from "express"
-import path from "path"
-const app=express();
+import express from "express";
+import path from "path";
+const app = express();
 
 // function middleware(req,resp,next){
 //     console.log("User is accessing "+req.url+" Page");
@@ -9,40 +9,39 @@ const app=express();
 
 // app.use(middleware);
 
-
-//  function AgeCheck(req,resp,next){
-    // if(!req.query.age || req.query.age<18)
-    // {
-    //     resp.send("You Cannot access this page");
-    // }
-    // else{
-    //     next();
-    // }
+//  function ipCheck(req,resp,next){
+//     const ip = (req.socket && req.socket.remoteAddress) || req.ip || '';
+//     console.log(ip);
+//     if (ip.includes('10.250.83.254')) {
+//         return resp.status(403).send("You Cannot access this page");
+//     }
+//     next();
 // }
+
+// app.use(ipCheck);
+
+// RouteMiddleWare use to apply middleware to specific route
+
+function AgeCheck(req, resp, next) {
+  if (!req.query.age || req.query.age < 18) {
+    resp.send("You Cannot access this page");
+  } else {
+    next();
+  }
+}
 
 // app.use(AgeCheck);
 
- function ipCheck(req,resp,next){
-    const ip = (req.socket && req.socket.remoteAddress) || req.ip || '';
-    console.log(ip);
-    if (ip.includes('10.250.83.254')) {
-        return resp.status(403).send("You Cannot access this page");
-    }
-    next();
-}
-
-app.use(ipCheck);
-
-app.get("/",(req,resp)=>{
-    resp.send("<h1>home page</h1>")
-})
-app.get("/login",(req,resp)=>{
-    resp.send("<h1>Login page</h1>")
-})
-app.post("/admin",(req,resp)=>{
-    resp.send("<h1>Admin page</h1>")
-})
-app.use((req,resp)=>{
-    resp.status(404).send("Page Not Found")
-})
+app.get("/", (req, resp) => {
+  resp.send("<h1>home page</h1>");
+});
+app.get("/login", (req, resp) => {
+  resp.send("<h1>Login page</h1>");
+});
+app.get("/admin", AgeCheck, (req, resp) => {
+  resp.send("<h1>Admin page</h1>");
+});
+app.use((req, resp) => {
+  resp.status(404).send("Page Not Found");
+});
 app.listen(4800);
