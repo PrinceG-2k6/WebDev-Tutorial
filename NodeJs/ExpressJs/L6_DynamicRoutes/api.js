1950d43f249e62c14adc21b1bf72d75decc8bcf7
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-16 19:53:39 +0530

    .

---
+++ NodeJs/ExpressJs/L6_DynamicRoutes/api.js
@@ -0,0 +1,22 @@
+import express, { urlencoded } from "express"
+import userData from './users.json' with {type:'json'}
+const app=express();
+app.get("/",(req,resp)=>{
+    console.log(userData)
+    resp.send(userData);
+})
+app.get("/user/:id",(req,resp)=>{
+    console.log(req.params.id);
+    const id =req.params.id;
+
+    let filterData = userData.filter((user)=>user.id==id)
+    resp.send(filterData);
+})
+app.get("/username/:name",(req,resp)=>{
+    console.log(req.params.name);
+    const name =req.params.name;
+
+    let filterData = userData.filter((user)=>user.name.toLowerCase()==name.toLowerCase())
+    resp.send(filterData);
+})
+app.listen(4800)
