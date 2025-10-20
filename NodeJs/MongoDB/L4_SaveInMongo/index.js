83cc500316a9265fb0a74962297e3970ec2b19f1
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-20 15:57:01 +0530

    .

--- NodeJs/MongoDB/L4_SaveInMongo/index.js
+++ NodeJs/MongoDB/L4_SaveInMongo/index.js
@@ -7,6 +7,7 @@ app.set("view engine","ejs")
 const dbname ="school";
 const url = "mongodb://localhost:27017"
 const client = new MongoClient(url);
+app.use(express.urlencoded({extended:true}))
 
 client.connect().then((connection)=>{
     const db =connection.db(dbname);
@@ -21,6 +22,18 @@ client.connect().then((connection)=>{
         const collection = db.collection("students"); 
         let students = await collection.find().toArray();
         resp.render('students',{students})
+    })
+    app.get('/add',async (req,resp)=>{
+        // const collection = db.collection("students"); 
+        // let students = await collection.find().toArray();
+        resp.render('form')
+    })
+    app.post('/addStudent', (req,resp)=>{
+        
+        const collection = db.collection("students"); 
+        const result = collection.insertOne(req.body);
+        console.log(result)
+        resp.render('addStudent')
     })
 })
 
