e3892684014a2a6ab7535c68de5b0e24ce2c9bcd
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-20 13:33:36 +0530

    .

--- NodeJs/MongoDB/L1_Connection/index.js
+++ NodeJs/MongoDB/L1_Connection/index.js
@@ -1,4 +1,4 @@
-import express from 'espress'
+import express from 'express'
 import {MongoClient} from 'mongodb'
 
 const dbname ="school";
@@ -6,11 +6,15 @@ const url = "mongodb://localhost:27017"
 const client = new MongoClient(url);
 
 async function dbConnect (){
-    await client.connect()
-    const db =client.db(dbname);
-    const collection= db.collection("students"); 
-    const result  =await collection.find().toArray()
-    console.log(result);
+    try {
+        await client.connect();
+        const db = client.db(dbname);
+        const collection = db.collection("students"); 
+        const result = await collection.find().toArray();
+        console.log(result);
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    }
 }
 
 dbConnect();
